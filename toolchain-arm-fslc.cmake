4bcfbaffe94933763269a6e45a82776da17dbe79
Jeremy <jeremy31880@gmail.com>
2021-07-30 16:12:26 +0200

    Add toolchain.cmake file and systemV init script example

---
+++ toolchain-arm-fslc.cmake
@@ -0,0 +1,13 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(CMAKE_SYSROOT /opt/fslc-framebuffer/2.6.2/sysroots/armv7at2hf-neon-fslc-linux-gnueabi)
+
+set(cross_prefix /opt/fslc-framebuffer/2.6.2/sysroots/x86_64-fslcsdk-linux/usr/bin/arm-fslc-linux-gnueabi/)
+set(CMAKE_C_COMPILER ${cross_prefix}arm-fslc-linux-gnueabi-gcc)
+set(CMAKE_CXX_COMPILER ${cross_prefix}arm-fslc-linux-gnueabi-g++)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
